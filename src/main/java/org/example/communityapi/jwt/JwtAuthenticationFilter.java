20ac3f1661e17323f4c76115a702e998d96735a0
Soyeong Park <mb535622@gmail.com>
2026-07-08 17:39:24 +0900

    feat: JWT 토큰 검증 및 인증 처리 로직 추가

--- src/main/java/org/example/communityapi/jwt/JwtAuthenticationFilter.java
+++ src/main/java/org/example/communityapi/jwt/JwtAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package org.example.communityapi.jwt;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -24,6 +26,17 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             FilterChain filterChain
     ) throws ServletException, IOException {
 
+        // 헤더에서 토큰 추출
+        String token = resolveToken(request);
+
+        // 토큰 검증
+        if (token != null && jwtProvider.validateToken(token)) {
+            // 토큰이 유효하면 Authentication 객체를 가져와서 SecurityContext에 저장
+            Authentication authentication = jwtProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
     }
 
     private String resolveToken(HttpServletRequest request) {
