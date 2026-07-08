f4f54bb32ff72d4f12df281d5eae12d42ab7b53d
Soyeong Park <mb535622@gmail.com>
2026-07-08 17:31:20 +0900

    feat: Authorization 헤더 기반 JWT 토큰 추출 로직 추가

--- src/main/java/org/example/communityapi/jwt/JwtAuthenticationFilter.java
+++ src/main/java/org/example/communityapi/jwt/JwtAuthenticationFilter.java
@@ -1,7 +1,36 @@
 package org.example.communityapi.jwt;
 
+import org.springframework.util.StringUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    private final JwtProvider jwtProvider;
+
+    public JwtAuthenticationFilter(JwtProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
 }
