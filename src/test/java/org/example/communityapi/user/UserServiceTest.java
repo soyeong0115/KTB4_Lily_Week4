53093ce53081e66f486c621f1716c8bfdad44b4d
Soyeong Park <mb535622@gmail.com>
2026-07-10 17:08:07 +0900

    test: UserService 중복 닉네임 프로필 수정 실패 테스트 추가

--- src/test/java/org/example/communityapi/user/UserServiceTest.java
+++ src/test/java/org/example/communityapi/user/UserServiceTest.java
@@ -12,6 +12,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
@@ -64,21 +65,23 @@ class UserServiceTest {
         // given
         User user = createUser();
 
+        String patchNickname = "중복닉네임";
+        String newProfileImage = "new-profile.png";
+
         given(userRepository.findById(USER_ID)).willReturn(Optional.of(user));
-        given(userRepository.existsByNickname("수정닉네임")).willReturn(false);
+        given(userRepository.existsByNickname(patchNickname)).willReturn(false);
 
-        UpdateProfileRequest request = createUpdateProfileRequest("수정닉네임", "new-profile-image.png");
-
+        UpdateProfileRequest request = createUpdateProfileRequest(patchNickname, newProfileImage);
 
         // when
         userService.updateProfile(USER_ID, request);
 
         // then
-        assertThat(user.getNickname()).isEqualTo("수정닉네임");
-        assertThat(user.getProfileImage()).isEqualTo("new-profile-image.png");
+        assertThat(user.getNickname()).isEqualTo(patchNickname);
+        assertThat(user.getProfileImage()).isEqualTo(newProfileImage);
 
         verify(userRepository).findById(USER_ID);
-        verify(userRepository).existsByNickname("수정닉네임");
+        verify(userRepository).existsByNickname(patchNickname);
     }
 
     private UpdateProfileRequest createUpdateProfileRequest(
@@ -91,6 +94,26 @@ class UserServiceTest {
     @Test
     @DisplayName("중복 닉네임이면 프로필 수정 실패")
     void updateProfile_DuplicatedNickname_ThrowsException() {
+        // given
+        User user = createUser();
+
+        String duplicatedNickname = "중복닉네임";
+        String newProfileImage = "new-profile.png";
+
+        given(userRepository.findById(USER_ID)).willReturn(Optional.of(user));
+        given(userRepository.existsByNickname(duplicatedNickname)).willReturn(true);
+
+        UpdateProfileRequest request = createUpdateProfileRequest(duplicatedNickname, newProfileImage);
+
+        // when + then
+        assertThatThrownBy(() -> userService.updateProfile(USER_ID, request))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThat(user.getNickname()).isEqualTo(NICKNAME);
+        assertThat(user.getProfileImage()).isEqualTo(PROFILE_IMAGE);
+
+        verify(userRepository).findById(USER_ID);
+        verify(userRepository).existsByNickname(duplicatedNickname);
     }
 
     @Test
