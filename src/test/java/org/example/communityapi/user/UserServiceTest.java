6f5e88cfd96eb4c2adbfb43b449d81b4c59d1aae
Soyeong Park <mb535622@gmail.com>
2026-07-10 16:52:20 +0900

    test: UserService 내 프로필 조회 성공 테스트 추가

--- src/test/java/org/example/communityapi/user/UserServiceTest.java
+++ src/test/java/org/example/communityapi/user/UserServiceTest.java
@@ -1,13 +1,60 @@
 package org.example.communityapi.user;
 
+import org.example.communityapi.user.dto.ProfileResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
+
+    private static final int USER_ID = 1;
+    private static final String EMAIL = "test@test.com";
+    private static final String PASSWORD = "encoded-password";
+    private static final String NICKNAME = "테스트";
+    private static final String PROFILE_IMAGE = null;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
 
     @Test
     @DisplayName("내 프로필 조회 성공")
     void getProfile_Success() {
+        // given
+        User user = createUser();
+
+        given(userRepository.findById(USER_ID)).willReturn(Optional.of(user));
+
+        // when
+        ProfileResponse response = userService.getProfile(USER_ID);
+
+        // then
+        assertThat(response.getEmail()).isEqualTo(EMAIL);
+        assertThat(response.getNickname()).isEqualTo(NICKNAME);
+        assertThat(response.getProfileImage()).isEqualTo(PROFILE_IMAGE);
+
+        verify(userRepository).findById(USER_ID);
+    }
+
+    private User createUser() {
+        return new User(
+                EMAIL,
+                PASSWORD,
+                NICKNAME,
+                PROFILE_IMAGE
+        );
     }
 
     @Test
