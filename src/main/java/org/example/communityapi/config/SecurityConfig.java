32825c47494dbc863b11fd785c94b81ff2c52435
Soyeong Park <mb535622@gmail.com>
2026-07-14 16:37:44 +0900

    feat: 이미지 업로드 보안 설정 추가

--- src/main/java/org/example/communityapi/config/SecurityConfig.java
+++ src/main/java/org/example/communityapi/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.GET, "/posts").permitAll()
                         .requestMatchers(HttpMethod.GET, "/posts/*").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/uploads/*").permitAll()
                         .anyRequest().authenticated()
                 )
 
