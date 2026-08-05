61dfcc046873dda5414f6177cfe43438686750c3
Soyeong Park <mb535622@gmail.com>
2026-08-05 15:40:46 +0900

    feat: 알림 리포지토리 구현

---
+++ src/main/java/org/example/communityapi/notification/NotificationRepository.java
@@ -0,0 +1,6 @@
+package org.example.communityapi.notification;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+}
